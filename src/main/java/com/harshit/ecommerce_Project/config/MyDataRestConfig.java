1a72831d79f83bbf3562b0e6882a44429376026f
HARSHIT KUMAR <harshitkumar@HARSHITs-Laptop.local>
2025-04-21 11:46:39 +0530

    Added Configuration for exposing Id

--- src/main/java/com/harshit/ecommerce_Project/config/MyDataRestConfig.java
+++ src/main/java/com/harshit/ecommerce_Project/config/MyDataRestConfig.java
@@ -2,26 +2,65 @@ package com.harshit.ecommerce_Project.config;
 
 import com.harshit.ecommerce_Project.entity.Product;
 import com.harshit.ecommerce_Project.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
 @Configuration
     public class MyDataRestConfig implements RepositoryRestConfigurer {
-        @Override
-        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
-            HttpMethod[] theUnsupportedActions= {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-            config.getExposureConfiguration()
-                    .forDomainType(Product.class) //Apply to Repo for : Class Name (for which you want to disable the endpoints)
-                    .withItemExposure((metadata,httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                    .withCollectionExposure((metadata,httpMethods) ->httpMethods.disable(theUnsupportedActions));
+    private EntityManager entityManager;
 
-            config.getExposureConfiguration()
-                    .forDomainType(ProductCategory.class) //Apply to Repo for : Class Name (for which you want to disable the endpoints)
-                    .withItemExposure((metadata,httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                    .withCollectionExposure((metadata,httpMethods) ->httpMethods.disable(theUnsupportedActions));
-        }
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class) //Apply to Repo for : Class Name (for which you want to disable the endpoints)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class) //Apply to Repo for : Class Name (for which you want to disable the endpoints)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //expose entity ids
+        //
+        //-get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        //-create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        //-get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+            System.out.println("###");
+            System.out.println(tempEntityType);
+        }
+        //-expose the entity ids for the array of entity types
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);// new Class[0] used to create an array from entityClasses of types Class.
+        //solved ClassCastEexception issue
+        config.exposeIdsFor(domainTypes);
+    }
+
+}
